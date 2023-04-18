bd2ddff6e9a6d482dfb034409c7584e4df71fcd0
Etienne <fischer.etienne@gmail.com>
2023-04-18 09:01:48 +0200

    add error and improve parsing

--- src/WikipediaScraper.php
+++ src/WikipediaScraper.php
@@ -17,8 +17,15 @@ class WikipediaScraper
     public function scrape(): array
     {
         $client = new Client();
-        $crawler = $client->request('GET', 'https://fr.wikipedia.org/wiki/' . $this->input);
+        $crawler = $client->request('GET', 'https://fr.wikipedia.org/w/index.php?search=' . $this->input . '&title=Sp%C3%A9cial:Recherche&profile=advanced&fulltext=1&ns0=1');
 
+        //first result
+        $link = $crawler->filter('ul.mw-search-results')->first()->filter('li')->first()->filter('a')->first()->attr('href');
+        if (empty($link)) {
+            throw new Exception('Aucun résultat trouvé pour ce terme');
+        }
+
+        $crawler = $client->request('GET', 'https://fr.wikipedia.org' . $link);
         //verify if a table with the class "infobox_v2" exists
         $crawler->filter('.infobox_v2')->each(function (Crawler $node) {
             if ($node->count() === 0) {
@@ -27,32 +34,48 @@ class WikipediaScraper
         });
         //get the table
         $table = $crawler->filter('.infobox_v2')->first();
-        //get all the th elements and their text
+        //get all the th elements who have a td and have a scope attribute 'row' and the td associated is not empty
         $th = $table->filter('th')->each(function (Crawler $node) {
-            return $node->text();
+            if ($node->siblings()->count() > 0 && $node->attr('scope') === 'row' && !empty($node->siblings()->first()->text())) {
+                return $node->text();
+            }
+            return null;
         });
 
-        var_dump($th);
-        //get all the td associated with the th and their text
+
+        $properties = array_filter($th);
+
+        var_dump($properties);
+
+        //get all the td associated with the th who have a scope attribute 'row'
         $td = $table->filter('th')->each(function (Crawler $node) {
-           $values = $node->siblings()->text();
-           return $this->parseAttributes($values);
+            if ($node->siblings()->count() > 0 && $node->attr('scope') === 'row') {
+                return $this->parseAttributes($node->siblings()->first()->text());
+            }
+            return null;
         });
 
-        var_dump($td);
+        $values = array_filter($td);
 
-        if (empty($th) || empty($td)) {
+        var_dump($values);
+
+        if (empty($properties) || empty($values)) {
             throw new Exception('Tableau incomplet');
         }
 
-        return array_combine($th, $td);
+        return array_combine($properties, $values);
 
     }
 
-    private function parseAttributes(string $attributeValue): mixed
+    private function parseAttributes(string $attributeValue): string|array
     {
         $attributeValue = str_replace(' et', ',', $attributeValue);
-        if (strpos($attributeValue, ',') !== false) {
+
+        if (preg_match('/\d,\d/', $attributeValue)) {
+            return $attributeValue;
+        }
+
+        if (str_contains($attributeValue, ',')) {
             return array_map('trim', explode(',', $attributeValue));
         }
 
